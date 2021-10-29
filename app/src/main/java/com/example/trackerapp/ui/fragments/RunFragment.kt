f8a1b0c21a141dcb3df58187e87f747893973bdd
diogomartins <dmartins_93@hotmail.com>
2021-10-29 05:16:07 +0100

    addFeature

--- app/src/main/java/com/example/trackerapp/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/example/trackerapp/ui/fragments/RunFragment.kt
@@ -1,7 +1,12 @@
 package com.example.trackerapp.ui.fragments
 
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.trackerapp.R
+import com.example.trackerapp.ui.viewmodels.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint //Inject
 class RunFragment : Fragment(R.layout.fragment_run) {
+    private val viewModel: MainViewModel by viewModels()
 }
--- app/src/main/java/com/example/trackerapp/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/example/trackerapp/ui/fragments/TrackingFragment.kt
@@ -1,7 +1,12 @@
 package com.example.trackerapp.ui.fragments
 
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.trackerapp.R
+import com.example.trackerapp.ui.viewmodels.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class TrackingFragment : Fragment(R.layout.fragment_tracking) {
+    private val viewModel: MainViewModel by viewModels()
 }
