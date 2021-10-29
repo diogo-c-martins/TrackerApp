d1b963adeca40ee30147df42ec857a71d860c654
diogomartins <dmartins_93@hotmail.com>
2021-10-29 16:41:09 +0100

    addService

--- app/src/main/java/com/example/trackerapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/trackerapp/ui/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.trackerapp.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.trackerapp.R
+import com.example.trackerapp.other.Constants.ACTION_SHOW_TRACKING_FRAGMENT
 import dagger.hilt.android.AndroidEntryPoint
 //Android Extensions
 import kotlinx.android.synthetic.main.activity_main.*
@@ -16,6 +18,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navigateToTrackingFragmentIfNeeded(intent)
 
         setSupportActionBar(toolbar)
         bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
@@ -29,7 +33,18 @@ class MainActivity : AppCompatActivity() {
                 }
 
             }
+    }
 
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        navigateToTrackingFragmentIfNeeded(intent)
+    }
 
+    //If main activity was destroyed and not service then it should relaunch
+    //Navigation Component Library
+    private fun navigateToTrackingFragmentIfNeeded(intent: Intent?){
+        if(intent?.action == ACTION_SHOW_TRACKING_FRAGMENT){
+            navHostFragment.findNavController().navigate(R.id.action_global_trackingFragment)
+        }
     }
 }
