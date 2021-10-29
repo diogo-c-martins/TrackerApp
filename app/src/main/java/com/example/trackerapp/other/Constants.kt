4f324a60adb70aa13a84ef9b450e8bb20ce1b079
diogomartins <dmartins_93@hotmail.com>
2021-10-29 06:34:14 +0100

    addFeature

--- app/src/main/java/com/example/trackerapp/other/Constants.kt
+++ app/src/main/java/com/example/trackerapp/other/Constants.kt
@@ -2,4 +2,5 @@ package com.example.trackerapp.other
 
 object Constants {
     const val RUNNING_DATABASE_NAME = "running_db" //7
+    const val REQUEST_CODE_LOCATION_PERMISSION = 0
 }
--- app/src/main/java/com/example/trackerapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/trackerapp/ui/MainActivity.kt
@@ -2,22 +2,34 @@ package com.example.trackerapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.trackerapp.R
-import com.example.trackerapp.db.RunDAO
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
+//Android Extensions
+import kotlinx.android.synthetic.main.activity_main.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    @Inject
-    lateinit var runDao: RunDAO
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("runDao", "RunDao: ${runDao.hashCode()}")
+
+        setSupportActionBar(toolbar)
+        bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
+
+        navHostFragment.findNavController()
+            .addOnDestinationChangedListener { _, destination, _ ->
+                when(destination.id){
+                    R.id.settingsFragment, R.id.runFragment, R.id.statisticsFragment ->
+                        bottomNavigationView.visibility = View.VISIBLE
+                    else -> bottomNavigationView.visibility = View.GONE
+                }
+
+            }
+
 
     }
 }
